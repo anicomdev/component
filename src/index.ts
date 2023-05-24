edffa7b6d747e2bf287080c6ea7328d9215fa8cf
duyduc2003 <dangduyducdangduyduc@gmail.com>
2023-05-24 19:59:33 +0700

    test 2

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import Button, { ButtonProps, ButtonDeleteProps, ButtonDelete } from './components/Button'
+
+export { Button, ButtonProps, ButtonDeleteProps, ButtonDelete }
--- src/index.tsx
+++
@@ -1,3 +0,0 @@
-import Button, { ButtonProps, ButtonDeleteProps } from './components/Button'
-
-export { Button, ButtonProps, ButtonDeleteProps }
